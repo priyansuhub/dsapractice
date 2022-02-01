438d5662016dad546af13fd31dd924b4ca93c21b
Priyansu Rath <priyansurath02@gmail.com>
2022-02-01 19:29:15 +0530

    modified subsequence

--- Subsequence.java
+++ Subsequence.java
@@ -1,3 +1,22 @@
+
+import java.util.*;
 public class Subsequence {
-    
+    public static void main(String args[]){
+        int arr[] = {3,1,2};
+        ArrayList<Integer> list = new ArrayList<>();
+        int ind = 0;
+        int n = 3;
+        generateSubsequence(ind, list, arr, n);
+    }
+
+    public static void generateSubsequence(int ind, ArrayList<Integer>list, int arr[],int n){
+        if(ind == n){
+            System.out.println(Arrays.deepToString(list.toArray()));
+            return;
+        }
+        list.add(arr[ind]);
+        generateSubsequence(ind+1, list, arr, n);
+        list.remove(list.size()-1);
+        generateSubsequence(ind+1, list, arr, n);
+    }
 }
